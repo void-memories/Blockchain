137883eefe045ff295414fa5c3a45debeb55b4ab
void-memories <kiethberry@gmail.com>
2020-03-29 13:33:33 +0530

    exceptions barebones

---
+++ headers/exceptions.h
@@ -0,0 +1,16 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+#include <vector>
+#include <map>
+#include <string>
+#include "dehash.h"
+
+class Exceptions{
+    int errorNo;
+    string errorMessage;
+
+    public:
+    void DispError();
+    void Exception(int no,string em);
+};
+#endif
---
+++ source/exceptions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include "../headers/exceptions.h"
+
+
+    
+    void DispError()
+    {
+
+    }
+
+    void Exceptions::Exception(int no,string em){
+
+    }
